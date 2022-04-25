1e811348b0fa3070537134610e2507b2d28936f9
moll <moll@alunos.utfpr.edu.br>
2022-04-25 10:36:50 -0300

    nunca vo termina 👍👍🤣🤣🤣🤣

--- TicTacToe/TicTacToe/Local.cs
+++ TicTacToe/TicTacToe/Local.cs
@@ -17,7 +17,6 @@ namespace TicTacToe
         {
             InitializeComponent();
         }
-
         private bool verifica(string jogador)
         {
             bool ganha = false;
@@ -58,37 +57,50 @@ namespace TicTacToe
             }
             return ganha;
         }
-
-        private void mensagem(string jogador, bool ganha) {
-            if (ganha)
-            {
-                MessageBox.Show($"O jogador {jogador} ganhou!");
-            }
+        private void desativa()
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            button7.Enabled = false;
+            button8.Enabled = false;
+            button9.Enabled = false;
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Button btn = (Button)sender;
 
             if (btn.Text == "")
             {
+                btn.Enabled = false;
                 if (turno)
                 {
                     string jogador = "X";
                     btn.Text = jogador;
                     turno = !turno;
                     bool ganha = verifica(jogador);
-                    mensagem(jogador, ganha);
+                    if (verifica(jogador))
+                    {
+                        MessageBox.Show($"O jogador {jogador} ganhou!");
+                        desativa();
+                    }
                 }
                 else
                 {
                     string jogador = "O";
                     btn.Text = jogador;
                     turno = !turno;
-                    bool ganha = verifica(jogador);
-                    mensagem(jogador, ganha);
+                    if (verifica(jogador))
+                    {
+                        MessageBox.Show($"O jogador {jogador} ganhou!");
+                        desativa();
+                    }
                 }
             }
         }
+
     }
 }
